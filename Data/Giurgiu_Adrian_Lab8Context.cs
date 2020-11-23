d5c7907f9c948c980c2e0752409f922a79d99047
Giurgiu Adrian <giurgiu.adrian1@yahoo.com>
2020-11-23 19:36:02 +0200

    Add project files.

---
+++ Data/Giurgiu_Adrian_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Giurgiu_Adrian_Lab8.Models;
+
+namespace Giurgiu_Adrian_Lab8.Data
+{
+    public class Giurgiu_Adrian_Lab8Context : DbContext
+    {
+        public Giurgiu_Adrian_Lab8Context (DbContextOptions<Giurgiu_Adrian_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Giurgiu_Adrian_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Giurgiu_Adrian_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Giurgiu_Adrian_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Giurgiu_Adrian_Lab8.Data;
+using Giurgiu_Adrian_Lab8.Models;
+
+namespace Giurgiu_Adrian_Lab8.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Giurgiu_Adrian_Lab8.Data.Giurgiu_Adrian_Lab8Context _context;
+
+        public IndexModel(Giurgiu_Adrian_Lab8.Data.Giurgiu_Adrian_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Category = await _context.Category.ToListAsync();
+        }
+    }
+}
